0a136daf06f6e32abba0fb93c7266e46bf85b4f4
ronentaich17 <ronentaich@gmail.com>
2022-08-19 14:54:11 -0700

    basic edits and updates
    added comments, clarifications and edited the stylesheet. I also deleted a text file that wasn't needed in the projects

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-const btnEl = document.querySelector(".btn")
-const inputEl = document.getElementById("input")
-const copyIconEl = document.querySelector(".fa-copy")
-const alertContainerEl = document.querySelector(".alert-container")
+const btnEl = document.querySelector(".btn") //the generate button
+const inputEl = document.getElementById("input") //where the password is generated
+const copyIconEl = document.querySelector(".fa-copy") //the copy icon shown on the right hand side of the "input"
+const alertContainerEl = document.querySelector(".alert-container") //the alert that pops up when a password is copied 
 
 btnEl.addEventListener("click", ()=> {
     createPassword();
@@ -17,8 +17,13 @@ copyIconEl.addEventListener("click",()=>{
     }
 });
 
+/*
+In the createPassword() function, a random number is chosen on line 29, and then the character at that index in
+the chars string will be assigned to the password on line 30 until the desired length is reached. Once this is done,
+the alert will pop up (the conditions of this alert are managed in function above)
+*/
 function createPassword(){
-    const chars = "0123456789abcdefghijklmnopqrstuvwxtz!@#$%^&*()_+?:{}[]ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    const chars = "0123456789abcdefghijklmnopqrstuvwxtz!@#$%^&*()_+?:{}[]ABCDEFGHIJKLMNOPQRSTUVWXYZ"; //the characters that may show up in the generated password
     const passwordLength = 13;
     let password = ""
     for (let index = 0; index < passwordLength; index++) {
@@ -30,7 +35,8 @@ function createPassword(){
 }
 
 function copyPassword(){
-    inputEl.select();
-    inputEl.setSelectionRange(0,9999); //for mobile users
+    //the two lines below simply highlights the password in the input area. Looking back, it doesn't look very pleasing so I commented it out
+    //inputEl.select();
+    //inputEl.setSelectionRange(0,9999); //for mobile users
     navigator.clipboard.writeText(inputEl.value); 
 }
